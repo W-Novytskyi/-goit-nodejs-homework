5a9ba05266bcceb068ca9403f95b18ada58182d4
W-Novytskyi <113388494+W-Novytskyi@users.noreply.github.com>
2023-06-25 18:34:08 +0300

    крок 2

---
+++ contacts.js
@@ -0,0 +1,63 @@
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.resolve(__dirname, "db", "contacts.json");
+
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    return JSON.parse(data);
+  } catch (error) {
+    return [];
+  }
+}
+
+async function getContactById(contactId) {
+  try {
+    const contacts = await listContacts();
+    const contact = contacts.find((c) => c.id === contactId);
+    return contact || null;
+  } catch (error) {
+    return null;
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const contacts = await listContacts();
+    const index = contacts.findIndex((c) => c.id === contactId);
+    if (index === -1) {
+      return null;
+    }
+    const [removedContact] = contacts.splice(index, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return removedContact;
+  } catch (error) {
+    return null;
+  }
+}
+
+async function addContact({ name, email, phone }) {
+  try {
+    const contacts = await listContacts();
+    const newContact = {
+      id: nanoid(),
+      name,
+      email,
+      phone,
+    };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return newContact;
+  } catch (error) {
+    return null;
+  }
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
