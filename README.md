ae17c8fb06472ee4a4388e46e1a890656452ba6f
W-Novytskyi <113388494+W-Novytskyi@users.noreply.github.com>
2023-06-25 21:25:13 +0300

    Крок 6

---
+++ README.md
@@ -0,0 +1,4 @@
+[listContacts] - (https://monosnap.com/file/JOXWx7JyEC05eI1cHEzjdYGHvmh2Fu)
+[getContactById] - (https://monosnap.com/file/06AqAZ83bjvXu5dxEN4FJImt0MXPFz)
+[addContact] - (https://monosnap.com/file/mSBS5QNGnq2tWxO4TMLIqKnfMSqeed)
+[removeContact] - (https://monosnap.com/file/0TUJ1ktCZ33fOOIbPuv4bFV4Gi37dS)
--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-// const {
-//   listContacts,
-//   getContactById,
-//   removeContact,
-//   addContact,
-// } = require("./contacts");
 const contacts = require("./contacts");
 const { Command } = require("commander");
 const program = new Command();
@@ -22,7 +16,7 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
     case "list":
       const list = await contacts.listContacts();
-      console.log(list);
+      console.table(list);
       break;
 
     case "get":
@@ -46,43 +40,3 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
 };
 
 invokeAction(argv);
-
-// invokeAction({ action: "listContacts" });
-// invokeAction({ action: "getContactById", id: "rsKkOQUi80UsgVPCcLZZW" });
-// invokeAction({
-//   action: "addContact",
-//   name: "John Cena",
-//   email: "johncena@gmail.com",
-//   phone: "+123456789",
-// });
-// invokeAction({ action: "removeContact", id: "oETMLYXTfhvMn0NisEp5E" });
-
-// const arr = hideBin(process.argv);
-// const { argv } = yargs(arr);
-// invokeAction(argv);
-
-// console.log(argv);
-
-// async function testContacts() {
-//   try {
-//     const contacts = await listContacts();
-//     console.log("All Contacts:", contacts);
-
-//     const contact = await getContactById("C9sjBfCo4UJCWjzBnOtxl");
-//     console.log("Contact with ID:", contact);
-
-//     const removedContact = await removeContact("rsKkOQUi80UsgVPCcLZZW");
-//     console.log("Removed Contact:", removedContact);
-
-//     const newContact = await addContact({
-//       name: "John Cena",
-//       email: "johncena@gmail.com",
-//       phone: "+123456789",
-//     });
-//     console.log("New Contact:", newContact);
-//   } catch (error) {
-//     console.error("Error:", error);
-//   }
-// }
-
-// testContacts();
